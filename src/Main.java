eebb04b6c28dfbfbfab45eed722e3b59ab140727
kavisu <kavisudaniel@gmail.com>
2024-03-13 05:59:17 +0300

    Simple fibonacci using recursion

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int numterms  = 5;
+
+        for (int i = 0; i < numterms; i++) {
+            System.out.print(FibonacciSeq.fibonacci(i) + " ");
+        }
+    }
+}
